5826519df6e715941dc71e1422dbd3f413206669
jacaremole <94930529+jacaremole@users.noreply.github.com>
2022-01-30 10:36:15 -0300

    Add files via upload

---
+++ PRO-c24-template2-main/sketch.js
@@ -0,0 +1,39 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var rubber;
+var steel;
+var stone;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    rubber = new Rubber(200,500,30);
+    steel = new Steel(578,500,40,40);
+    stone = new Stone(900,500,40,40);
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    rubber.display();
+    steel.display();
+    stone.display();
+
+    
+ 
+}
